9bacbb56f9eb1fe8b531a9d0280d5c19ed425b30
Nextor <nestor.panu@gmail.com>
2022-06-23 22:36:56 -0500

    Entity project added for models and DTOs

---
+++ BrowserChat.Entity/DTO/RoomReadDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrowserChat.Entity.DTO
+{
+    public class RoomReadDTO
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
