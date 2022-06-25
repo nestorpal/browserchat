11087cb6775dd8860ddc47f498fca614b0411ccd
Nextor <nestor.panu@gmail.com>
2022-06-25 10:55:54 -0500

    Client Project base template added

---
+++ BrowserChat.Client/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BrowserChat.Client.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
