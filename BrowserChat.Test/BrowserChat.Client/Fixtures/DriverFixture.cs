02097979ac78fb9097b7195a5136cd85de584351
Nestor P <nestor.panu@gmail.com>
2026-05-26 12:11:36 -0500

    feat: upgrade test project and add MS Edge compatibility

--- BrowserChat.Test/BrowserChat.Client/Fixtures/DriverFixture.cs
+++ BrowserChat.Test/BrowserChat.Client/Fixtures/DriverFixture.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BrowserChat.Test.BrowserChat.Client.Fixtures
+﻿namespace BrowserChat.Test.BrowserChat.Client.Fixtures
 {
     public abstract class DriverFixture : IDisposable
     {
---
+++ BrowserChat.Test/BrowserChat.Client/Fixtures/EdgeDriverFixture.cs
@@ -0,0 +1,12 @@
+﻿namespace BrowserChat.Test.BrowserChat.Client.Fixtures
+{
+    public class EdgeDriverFixture : DriverFixture
+    {
+        protected override void InitializeDriver()
+        {
+            Driver.Start(BrowserType.Edge);
+        }
+
+        public override int WaitForElementTimeout => 30;
+    }
+}
